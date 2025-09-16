7cb5a46d342e27cf05c794724fe8aa819531988c
jeffersonolvera <olverasuarezjeff@gmail.com>
2025-09-15 20:08:22 -0500

    Update: Refactorización para no mostrar la frase anterior

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const FRASES = ['Frase 1', 'Frase 2', 'Frase 3', 'Frase 4', 'Frase 5'];
 
 const fraseDiv = document.getElementById('frase');
+const button = document.getElementById('cambiar-frase');
 
 fraseDiv.innerText = FRASES[0];
 
-const button = document.getElementById('cambiar-frase');
+const obtenerFraseAleatoria = (fraseActual) => {
+  const frasesDisponibles = FRASES.filter((frase) => frase !== fraseActual);
 
-button.addEventListener('click', () => {
-  const nuevaFrase = FRASES[Math.floor(Math.random() * FRASES.length)];
+  return frasesDisponibles[
+    Math.floor(Math.random() * frasesDisponibles.length)
+  ];
+};
+
+const cambiarFrase = () => {
+  const nuevaFrase = obtenerFraseAleatoria(fraseDiv.innerText);
   fraseDiv.innerText = nuevaFrase;
-});
+};
+
+button.addEventListener('click', cambiarFrase);
 
 console.log(FRASES);

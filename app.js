6510d4138ec5fc1db836ad7a90353b702ef2bb62
Erding Flores <israelflores408@gmail.com>
2025-09-17 19:54:43 -0500

    change color with new message

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const FRASES = ["Cree en ti y todo será posible.",
 
 const fraseDiv = document.getElementById('frase');
 const button = document.getElementById('cambiar-frase');
+const contenedor = document.getElementById('container');
 
 fraseDiv.innerText = FRASES[0];
 
@@ -20,7 +21,21 @@ const obtenerFraseAleatoria = (fraseActual) => {
 const cambiarFrase = () => {
   const nuevaFrase = obtenerFraseAleatoria(fraseDiv.innerText);
   fraseDiv.innerText = nuevaFrase;
+  changeColor()
 };
+
+function changeColor() {
+  const red = getRandomInt(256)
+  const green = getRandomInt(256)
+  const blue = getRandomInt(256)
+  const color = `rgb(${red} ${green} ${blue})`
+  contenedor.style.backgroundColor = color
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 
 button.addEventListener('click', cambiarFrase);
 
